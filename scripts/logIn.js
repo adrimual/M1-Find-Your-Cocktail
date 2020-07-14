68c0895a77b735a00d41f0cab37728c4fe91881b
adrimual <adrimual14@gmail.com>
2020-07-14 16:38:37 +0200

    12th commit, some minor changes

---
+++ scripts/logIn.js
@@ -0,0 +1,42 @@
+'use strict';
+
+class Login {
+    constructor() {
+        this.emailInput = document.querySelector("#email");
+        this.passwordInput = document.querySelector("#password");
+        this.loginButton = document.querySelector("#login-button");
+        this.messageContainer = document.querySelector(".message-container");
+    }
+
+    submit = (event) => {
+        event.preventDefault();
+        const usersDB = db.getAllUsers();
+        const email = this.emailInput.value;
+        const password = this.passwordInput.value;
+        const user = usersDB.find((userObj) => {
+            if (userObj.email === email && userObj.password === password) {
+                return true;
+            }
+        })
+        this.showMessage(user);
+    }
+    showMessage = (user) => {
+        this.messageContainer.innerHTML = "";
+        const message = document.createElement('p');
+        if (user) {
+            message.innerHTML = `Welcome, ${user.email}`;
+            message.classList.add("correct-message");
+        } else {
+            message.innerHTML = 'Incorrect user or password';
+        }
+        this.messageContainer.appendChild(message);
+
+        if (user) this.redirect();
+    }
+    redirect = () => {
+        setTimeout(() => location.assign('/index.html'), 2000);
+    }
+
+}
+const login = new Login();
+login.loginButton.addEventListener("click", login.submit);
