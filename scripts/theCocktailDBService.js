65aad8097814efab51d0ab2c077bac0197c0b67b
adrimual <adrimual14@gmail.com>
2020-07-16 01:01:17 +0200

    1th commit, I get the images and title from the api, solved some big problems.

--- scripts/theCocktailDBService.js
+++ scripts/theCocktailDBService.js
@@ -1,28 +1,53 @@
 'use strict';
-const inputValue = document.querySelector("#search-bar").value;
-const searchBtn = document.querySelector(".search-btn");
-const getCocktailByName = (cocktails) => {
+
+var searchBtn = document.querySelector(".search-btn");
+
+const getCocktailByName = () => {
+    var cocktails = document.querySelector("#search-bar").value;
     console.log(cocktails);
-    const section = document.querySelector(".cocktail-by-name");
+    var section = document.querySelector("#cocktail-by-name");
+
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktails}`)
         .then((response) => {
             console.log(response);
             return response.json();
         })
         .then((cocktail) => {
-            console.log(cocktail);
-            const article = document.createElement("article");
-            for (let i = 0; i < cocktails.length; i++) {
-                if (inputValue == cocktails.strDrink) {
-                    article.innerHTML = `
-                        <img src="${cocktail.strDrinkThumb}" alt="${cocktail.strDrink}"/>
-                        <h3>${cocktail.strDrink}</h3>
+            var article = document.createElement("article");
+            for (let i = 0; i < cocktail.drinks.length; i++) {
+                console.log(cocktail.drinks[i].strDrinkThumb);
+                // if (cocktails == cocktails.strDrink) {
+                section.innerHTML = section.innerHTML + `
+                        <img src="${cocktail.drinks[i].strDrinkThumb}" alt="${cocktail.drinks[i].strDrink}"/>
+                        <h3>${cocktail.drinks[i].strDrink}</h3>
                         `;
-                    section.appendChild(article);
-                }
+                // }
             }
         })
         .catch((err) => {})
 }
+// const getAllCocktails = (cocktails) => {
+//     console.log("cocktails");
+//     const section = document.querySelector(".cocktail-by-name");
+//     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktails}`)
+//         .then((response) => {
+//             console.log(response);
+//             return response.json();
+//         })
+//         .then((cocktail) => {
+//             console.log(cocktail);
+//             const article = document.createElement("article");
+//             for (let i = 0; i < cocktails.length; i++) {
+//                 if (inputValue == cocktails.strDrink) {
+//                     article.innerHTML = `
+//                         <img src="${cocktail.strDrinkThumb}" alt="${cocktail.strDrink}"/>
+//                         <h3>${cocktail.strDrink}</h3>
+//                         `;
+//                     section.appendChild(article);
+//                 }
+//             }
+//         })
+//         .catch((err) => {})
+// }
 
-searchBtn.addEventListener("click", () => getCocktailByName(inputValue))
+searchBtn.addEventListener("click", () => getCocktailByName())
