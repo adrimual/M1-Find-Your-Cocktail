3528623a3f5a16aab4fe2d2dc7a40c14ee25ed85
adrimual <adrimual14@gmail.com>
2020-07-16 12:25:44 +0200

    20th commit or so, find cocktail works properly, need to add link, style etc

--- scripts/theCocktailDBService.js
+++ scripts/theCocktailDBService.js
@@ -3,51 +3,57 @@
 var searchBtn = document.querySelector(".search-btn");
 
 const getCocktailByName = () => {
-    var cocktails = document.querySelector("#search-bar").value;
+    var cocktails = document.querySelector("#search-bar");
     console.log(cocktails);
     var section = document.querySelector("#cocktail-by-name");
-
+    section.innerHTML = "";
+    if (cocktails.value != "") {
+        fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktails.value}`)
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((cocktail) => {
+                if (cocktail.drinks) {
+                    for (let i = 0; i < cocktail.drinks.length; i++) {
+                        console.log(cocktail.drinks[i].strDrink);
+                        if (cocktail.drinks[i].strDrink.toLowerCase().includes(cocktails.value.toLowerCase())) {
+                            console.log(cocktail.drinks[i].strDrinkThumb);
+                            section.innerHTML = section.innerHTML + `<div class="image-container">
+                    <img src="${cocktail.drinks[i].strDrinkThumb}" alt="${cocktail.drinks[i].strDrink}"/>
+                    <h3>${cocktail.drinks[i].strDrink}</h3></div>`
+                        }
+                    }
+                    cocktails.value = "";
+                } else {
+                    const errorP = document.createElement("p");
+                    errorP.innerHTML = "Not results found";
+                    section.appendChild(errorP);
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+}
+const getCocktailsByIngredients = () => {
+    var cocktails = document.querySelector("#search-bar").value;
+    console.log(ingredient);
+    var section = document.querySelector("#cocktail-by-ingredient");
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktails}`)
         .then((response) => {
             console.log(response);
             return response.json();
         })
         .then((cocktail) => {
-            var article = document.createElement("article");
+
             for (let i = 0; i < cocktail.drinks.length; i++) {
                 console.log(cocktail.drinks[i].strDrinkThumb);
-                // if (cocktails == cocktails.strDrink) {
-                section.innerHTML = section.innerHTML + `
-                        <img src="${cocktail.drinks[i].strDrinkThumb}" alt="${cocktail.drinks[i].strDrink}"/>
-                        <h3>${cocktail.drinks[i].strDrink}</h3>
-                        `;
-                // }
+                section.innerHTML = section.innerHTML + `<div class="image-container">
+                <img src="${cocktail.drinks[i].strDrinkThumb}" alt="${cocktail.drinks[i].strDrink}"/>
+                <h3>${cocktail.drinks[i].strDrink}</h3></div>`
             }
         })
         .catch((err) => {})
 }
-// const getAllCocktails = (cocktails) => {
-//     console.log("cocktails");
-//     const section = document.querySelector(".cocktail-by-name");
-//     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktails}`)
-//         .then((response) => {
-//             console.log(response);
-//             return response.json();
-//         })
-//         .then((cocktail) => {
-//             console.log(cocktail);
-//             const article = document.createElement("article");
-//             for (let i = 0; i < cocktails.length; i++) {
-//                 if (inputValue == cocktails.strDrink) {
-//                     article.innerHTML = `
-//                         <img src="${cocktail.strDrinkThumb}" alt="${cocktail.strDrink}"/>
-//                         <h3>${cocktail.strDrink}</h3>
-//                         `;
-//                     section.appendChild(article);
-//                 }
-//             }
-//         })
-//         .catch((err) => {})
-// }
-
 searchBtn.addEventListener("click", () => getCocktailByName())
