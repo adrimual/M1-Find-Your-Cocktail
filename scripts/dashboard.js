434f138c42b432419bff7ec0b53819d39efcebd4
adrimual <adrimual14@gmail.com>
2020-07-17 10:51:36 +0200

    23rd commit, only about us remaining

--- scripts/dashboard.js
+++ scripts/dashboard.js
@@ -1,3 +1,7 @@
+const cocktailElement = document.querySelector(".cocktail");
+const ingredientsElement = document.querySelector(".ingredients ul");
+const instructionsElement = document.querySelector(".instructions");
+
 const displayResult = (cocktailName) => {
     var section = document.querySelector("#cocktail-by-name");
     fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${cocktailName}`)
@@ -12,48 +16,18 @@ const displayResult = (cocktailName) => {
                     cocktail.drinks[0].strIngredient10, cocktail.drinks[0].strIngredient11, cocktail.drinks[0].strIngredient12,
                     cocktail.drinks[0].strIngredient13, cocktail.drinks[0].strIngredient14, cocktail.drinks[0].strIngredient15
                 ];
-                section.innerHTML =
-                    section.innerHTML + `
-                    <div class="image-container">
-                        <div class ="center">
-                            <table style="width:100%">
-                                <tbody>
-                                    <tr>
-                                        <td style="width:35%">
-                                            <h3>${cocktail.drinks[0].strDrink}</h3>
-                                        </td>
-                                        <td style="width:60%">
-                                            <h3>Ingredients</h3>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td style="width:35%; vertical-align:top;">
-                                            <img src="${cocktail.drinks[0].strDrinkThumb}" alt="${cocktail.drinks[0].strDrink}"/>
-                                        </td>
-                                        <td style="width:60%; vertical-align:top;">
-                                            <table style="width:100%">
-                                                <tbody>
-                                                    <tr>`;
+                console.log(ingredients);
+                cocktailElement.innerHTML = `
+                                <h3>${cocktail.drinks[0].strDrink}</h3>
+                                <img src="${cocktail.drinks[0].strDrinkThumb}" alt="${cocktail.drinks[0].strDrink}"/>`
                 for (var i = 0; i < ingredients.length; i++) {
                     if (ingredients[i]) {
                         console.log(ingredients[i]);
-                        section.innerHTML = section.innerHTML + `<td>` + ingredients[i] + `</td>`;
-                        console.log(section.innerHTML);
-                    } else {
-                        section.innerHTML =
-                            section.innerHTML +
-                            `</tr>
-                                                </tbody>
-                                            </table>
-                                        </td>
-                                </tbody>
-                            </table>
-                        </div>
-                    </div>
-                    `
-                        break;
+                        ingredientsElement.innerHTML = ingredientsElement.innerHTML + `<li> ${ingredients[i]}</li>`;
+                        console.log(ingredientsElement.innerHTML);
                     }
                 }
+                instructionsElement.innerHTML = cocktail.drinks[0].strInstructions;
             }
         })
         .catch((err) => console.log(err))
